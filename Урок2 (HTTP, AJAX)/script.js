8aeaa95c22aaa6891ab553824427010f9110c755
neox56 <neox56@gmail.com>
2019-07-24 18:46:43 +0400

    добавлен генератор рандомных пользовательских данных с выводом автара и информации о человеке.

--- Урок2 (HTTP, AJAX)/script.js
+++ Урок2 (HTTP, AJAX)/script.js
@@ -1,4 +1,5 @@
 let getData = document.querySelector('#btn-1');
+let getPeople = document.querySelector('#btn-2');
 
 getData.addEventListener('click', function () {
 	let request = new XMLHttpRequest();
@@ -10,4 +11,32 @@ getData.addEventListener('click', function () {
 			console.log(data);
 		};
 	});
+});
+
+
+getPeople.addEventListener('click', function () {
+	let request = new XMLHttpRequest();
+	request.open('GET', 'https://api.randomuser.me/1.0/?results=12&nat=gb,us&inc=gender,name,location,email,phone,picture', true);
+	request.send();
+
+	request.addEventListener('readystatechange', function (data) {
+		if (request.readyState === 4 && request.status == 200) {
+			let data = JSON.parse(request.response);
+			let output = '<h2>People</h2>';
+
+			data.results.forEach(function (data) {
+				output += `
+                 <ul class="wrap-list">
+                 <li> <img src = "${data.picture.medium} "> </li>
+                 <li> Name: ${data.name.title} ${data.name.first} ${data.name.last}</li>
+ 				 <li> Gender: ${data.gender}</li>
+				 <li> Email: ${data.email}</li>
+                 <li> Phone: ${data.phone}</li>
+                 </ul>
+                `;
+			});
+			document.querySelector('#box').innerHTML = output;
+		};
+	});
+
 });

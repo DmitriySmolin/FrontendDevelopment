78d93bb9dfc35b7c32fb419c79743dff4df9d1a6
neox56 <neox56@gmail.com>
2019-08-23 13:22:52 +0400

    [6]Удален компонент Portfolio, добавлен новый компонент Navigation

---
+++ Урок6(React)/portfolio/src/Component/Navigation/Navigation.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+// import './App.css';
+import './Navigation.css';
+class Navigation extends Component {
+    render() {    
+    	const sections = ['Главная','Обо мне','Услуги','Портфолио','Контакты'];
+    	const navLinks = sections.map(section => {
+    		return(
+    			<li><a href={'#' + section}>{section}</a></li>
+    		)
+    	})
+       return (
+               <nav>
+               		<h2 className="logo">{this.props.LogoTitle}</h2>
+					<ul>
+						{navLinks}
+					</ul>
+               	</nav>
+
+               
+        );
+    }
+}
+
+export default Navigation;

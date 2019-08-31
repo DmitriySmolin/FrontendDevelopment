ffcfa8fd64a441731016af2abb420770c6f77341
neox56 <neox56@gmail.com>
2019-08-31 13:55:30 +0400

    Updates

---
+++ precache-manifest.b520f9e30bc66b568eb3b89978b43f6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f2d4f7875156a4bcc722f9ef7f5f950",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "4a5f03a29cd6f2eb3519",
+    "url": "/deploy-react/static/css/main.2f15c3ee.chunk.css"
+  },
+  {
+    "revision": "cc95cf52ff9118fd5293",
+    "url": "/deploy-react/static/js/2.51678b07.chunk.js"
+  },
+  {
+    "revision": "4a5f03a29cd6f2eb3519",
+    "url": "/deploy-react/static/js/main.567c7b70.chunk.js"
+  },
+  {
+    "revision": "ddfcb83cc4c22f6c1395",
+    "url": "/deploy-react/static/js/runtime~main.cce89d36.js"
+  }
+]);
